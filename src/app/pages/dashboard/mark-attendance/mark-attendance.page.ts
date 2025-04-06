082593cb76ec1ed16982cfdd5ab9e59a958b81ba
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-04-07 03:16:28 +0530

    done

--- src/app/pages/dashboard/mark-attendance/mark-attendance.page.ts
+++ src/app/pages/dashboard/mark-attendance/mark-attendance.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { MyCustomPlugin } from 'my-custom-plugin/src';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { Camera } from '@capacitor/camera';
+import { MyCustomPlugin } from 'my-custom-plugin/src';
 
 
 
@@ -29,7 +29,7 @@ export class MarkAttendancePage implements OnInit {
 
   // async proceed(){
   //   await MyCustomPlugin.startFaceDetection({ image: "This is my first plugin" }).then((res:any)=>{
-  //     alert("Return value is "+JSON.stringify(res.value));
+  //     alert("Return value is "+JSON.stringify(res.image));
   //   }).catch((err:any)=>{alert(JSON.stringify(err))});
   // }
 
@@ -39,12 +39,22 @@ export class MarkAttendancePage implements OnInit {
       const status = await Camera.requestPermissions();
       if (status.camera === 'granted') {
         const result = await MyCustomPlugin.startFaceDetection();
-        this.capturedImage = `data:image/jpeg;base64,${result.image}`;
+        console.log('Raw result from plugin:', result);
+        alert('Got result: ' + JSON.stringify(result));
+  
+        if (result.image.startsWith('data:image')) {
+          this.capturedImage = result.image;
+        } else {
+          this.capturedImage = `data:image/jpeg;base64,${result.image}`;
+        }
+  
+        console.log('capturedImage set:', this.capturedImage);
+        alert('Captured image set in variable');
       } else {
         alert('Camera permission is required to take a photo.');
       }
     } catch (err) {
-      this.capturedImage=`Detection failed ${err}`;
+      this.capturedImage = `Detection failed ${err}`;
       console.error('Detection failed', err);
     }
   }
