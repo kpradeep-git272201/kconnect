d8e984f822690d6eb3b6f7db336ac2b32e1dd1e3
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-04-09 15:35:25 +0530

    done

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -42,31 +42,15 @@ export class CommonService {
   getLogin(data: { username: string; password: string }): Observable<any> {
     const url = `${AppConfig.BASE_API}${AppConfig.ENDPOINTS.login}`;
   
-    if (this.isMobile()) {
-      // ✅ Native headers must be plain object
-      const nativeHeaders = {
-        'Content-Type': 'application/json'
-      };
-      console.log('📡 [Native] URL:', url);
-      console.log('📦 [Native] Data:', data);
-      console.log('🧾 [Native] Headers:', nativeHeaders);
-      return from(this.httpNative.post(url, data, nativeHeaders)).pipe(
-        catchError(error => {
-          alert('❌ Native Error: ' + JSON.stringify(error));
-          return throwError(() => error);
-        })
-      );
-    } else {
-      const webHeaders = new HttpHeaders({
-        'Content-Type': 'application/json'
-      });
-  
-      return this.http.post(url, data, { headers: webHeaders }).pipe(
-        catchError(error => {
-          alert('❌ Web Error: ' + JSON.stringify(error));
-          return throwError(() => error);
-        })
-      );
-    }
+    const webHeaders = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post(url, data, { headers: webHeaders }).pipe(
+      catchError(error => {
+        alert('Web Error: ' + JSON.stringify(error));
+        return throwError(() => error);
+      })
+    );
   }
 }
